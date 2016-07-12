fded2b5c8e6d39a6663de19d5de0aa13be07ad7d
Dominic Muttel <dominic.muttel@gmail.com>
2016-07-11 19:20:06 -0600

    scss structure changes and gulp refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var connect = require('gulp-connect');
-var sass = require('gulp-sass');
-var sass = require('gulp-sass');
-// var compass = require('gulp-compass');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
 
 
 // gulp.task('default', function () {
@@ -16,85 +15,92 @@ var browserSync = require('browser-sync').create();
 // 		}))
 // 		.pipe(gulp.dest('dist'));
 // });
+var src = {
+    scss: 'scss/**/*.scss',
+    css:  'css/*.css',
+    html: '*.html',
+    cssDest: 'css',
+    productionDest: 'production'
+};
+//=======================================
+// concat all .js
+// js/main.js -> production/app.js
+//=======================================
+gulp.task('concatScripts', function(){
+  gulp.src('js/main.js') // <- add other js files in array
+  //concats files to one file
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest(src.productionDest))
+});
 
-// gulp test
-// gulp.task('hello', function(){
-//   console.log('yo d');
-// })
+//=======================================
+// minify all .js
+// production/app.js -> to production/app.js
+//=======================================
+gulp.task('minifyScripts', function(){
+  gulp.src('production/app.js')
+  // minifies code
+  .pipe(uglify())
+  // rename file to app.min.js
+  .pipe(rename('app.min.js'))
+  .pipe(gulp.dest(src.productionDest));
+});
 
-// configure connect task
-gulp.task('connect', function() {
-  connect.server({
-    livereload: true
-  });
+//=======================================
+// sass -> css
+//=======================================
+
+gulp.task('sass', function(){
+    gulp.src(src.scss)
+    .pipe(sass()) // Using gulp-sass
+    .pipe(autoprefixer('last 2 versions')) // auto-prefix css
+    .pipe(gulp.dest(src.cssDest))
+});
+
+// configure browserSync
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
 });
 
 gulp.task('html', function () {
-  gulp.src('*.html')
-    .pipe(connect.reload())
-    // browserSync
+    gulp.src(src.html)
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
 gulp.task('css', function () {
-  gulp.src('css/*.css')
-    .pipe(connect.reload());
-});
-
-
-// configure jshint task
-gulp.task('jshint', function() {
-  return gulp.src('js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    // browserSync
+    gulp.src('css/*.css')
     .pipe(browserSync.reload({
       stream: true
     }))
 });
+/////////////////////////////////////////
+gulp.task('serve', ['sass'], function() {
 
-// configure sass
-gulp.task('sass', function(){
-  return gulp.src('scss/**/*.scss')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('css'))
-    // browserSync
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    browserSync.init({
+        server: ""
+    });
+
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch(src.html).on('change', browserSync.reload);
 });
-// configure compass
-// gulp.task('styles', function() {
-//   return gulp.src('scss/**/*.scss')
-//     .pipe(compass({
-//       css: 'css',
-//       sass: 'scss',
-//       image: 'images'
-//     }))
-//     .pipe(gulp.dest('html/css'))
-// })
+/////////////////////////////////////////
 
-// configure browserSync
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: ''
-    },
-  })
-})
+
 
 
 // configure which files to watch and what tasks to use on file changes
-gulp.task('watch', ['browserSync', 'sass'], function() {
-  gulp.watch('js/*.js', ['jshint'], browserSync.reload);
-  gulp.watch(['*.html'], ['html'], browserSync.reload);
-  gulp.watch(['css/*.css'], ['css']);
-  gulp.watch('scss/**/*.scss', ['sass']);
-
-});
+// gulp.task('watch', ['browserSync', 'sass'], function() {
+//   gulp.watch(['*.html'], ['html']);
+//   gulp.watch(['css/*.css'], ['css']);
+  // gulp.watch(src.scss, ['sass']);
+//
+// });
 
 // default task!
-gulp.task('default', ['watch', 'connect']);
+gulp.task('default', ['serve']);
