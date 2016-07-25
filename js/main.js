66391fb605a26a3cc97810d5b7ea8a07804949ec
Dominic Muttel <dominic.muttel@gmail.com>
2016-07-25 17:26:27 -0600

    security / desktop nav member

--- js/main.js
+++ js/main.js
@@ -67,4 +67,59 @@ $(document).on('ready', function() {
     //     }
     //     $box.slideDown(500);
     // });
-});
+//---------------------------------------------------
+// AJAX for members / Temp login
+//---------------------------------------------------
+// var membersUrl = "../data/member.html";
+// $.getJSON(membersUrl, function (response) {
+//     });
+// SHOW MEMBER LOGIN
+    $("#member-login").click( function(){
+        $("#contact").toggle();
+        $("#login-form").toggle();
+    });
+// LOGOUT
+    $("#members").click("input:button[name='logout']", function() {
+        $("#members").toggle();
+        $("#public").toggle();
+    });
+// LOGIN
+    $("input:button[name='submit']").click(function(){
+        var name = $("input:text[name='name']").val();
+        var pass = $("input:text[name='password']").val();
+        // var insertAfter = $()
+        if( name === "pirate" && pass === "16") {
+            // $(".main-mobile-nav").hide();
+            // $("mobile-nav").html(
+            //     "<li><a <a href='#announcments'>ANNOUNCMENTS</a></li>" +
+            //     "<li><a href='#announcments'>MEETINGS / DUES</a></li>" +
+            //     "<li><a href='#announcments'>RULES</a></li>" +
+            //     "<li>SHOW:</li>" +
+            //     "<li><a href='#announcments'>PRE</a></li>" +
+            //     "<li><a href='#announcments'>DURING</a></li>" +
+            //     "<li><a href='#announcments'>POST</a></li>" +
+            //     "<li><a href='#announcments'>CONTACT LIST</a></li>" +
+            //     "<li><a href='#announcments'>LIASONS</a></li>"
+            // );
+            $(".main-nav").toggle();
+            $(".desktop-nav").html(
+                "<span><a href='#announcments'>ANNOUNCMENTS</a></span>" +
+                "<span><a href='#announcments'>MEETINGS / DUES</a></span>" +
+                "<span><a href='#announcments'>RULES</a></span>" +
+                "<span>SHOW:</span>" +
+                "<span><a href='#announcments'>PRE</a></span>" +
+                "<span><a href='#announcments'>DURING</a></span>" +
+                "<span><a href='#announcments'>POST</a></span>" +
+                "<span><a href='#announcments'>CONTACT LIST</a></span>" +
+                "<span><a href='#announcments'>LIASONS</a></span>"
+            );
+            $(".background").addClass('background-grey');
+            $("#public").toggle();
+            $("#members").load("../data/member.html");
+        } else {
+            alert("Incorrect Password");
+            $("#contact").toggle();
+            $("#login-form").toggle();
+        }
+    });
+});// END ONLOAD
